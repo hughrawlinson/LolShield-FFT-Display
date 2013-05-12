a4a1425b733d800d429a0fd4b8dea0ebc70d4c07
Hugh Rawlinson <hugh@codeoclock.net>
2013-05-12 16:46:55 +0100

    optimizing idiocy level

--- lolshieldfftdisplayarduino/lolshieldfftdisplayarduino.ino
+++ lolshieldfftdisplayarduino/lolshieldfftdisplayarduino.ino
@@ -1,41 +1,83 @@
+#include <SoftwareSerial.h>
 #include <Charliplexing.h>
 #include <Figure.h>
 #include <Font.h>
 #include <Myfont.h>
 
-#include <SoftwareSerial.h>
-SoftwareSerial portOne(2, 3);
-int count;
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
 
-void setup(){
-  LedSign::Init(DOUBLE_BUFFER);
+void setup() {
+  LedSign::Init();
+  // initialize serial:
   Serial.begin(9600);
-   while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
-  // Start each software serial port
-  portOne.begin(9600);
+  // reserve 200 bytes for the inputString:
+  inputString.reserve(200);
 }
 
-void loop(){
-  LedSign::Set(5, 5, 1);
-  portOne.listen();
-  while (portOne.available() > 0) {
-    char inByte = portOne.read();
-    int amp = inByte - '0';
-    Serial.write(inByte);
-    for(int i = 0; i < 9; i++){
-      LedSign::Set(count, i , 0);
+void loop() {
+  // print the string when a newline arrives:
+  if (stringComplete) {
+
+    String col = getValue(inputString, ' ', 1);
+    String am = getValue(inputString, ' ', 0);
+    
+    int column = stringToInt(col);
+    int amp = stringToInt(am);
+    
+    Serial.println(column);
+    Serial.println(amp);
+    
+    amp = 9 - amp;
+    
+    for(int i = 0; i < 9; i++) {
+      //turn off lights
+      LedSign::Set(column, i, 0);
+      if(i>amp){
+        LedSign::Set(column, i, 1);
+      }
     }
-    for(int i = 0; i < 5; i++){
-      LedSign::Set(count, i , 1);
-    }
-    //count++;
+    
+    // clear the string:
+    inputString = "";
+    stringComplete = false;
   }
 }
 
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read(); 
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    } 
+  }
+}
 
+String getValue(String data, char separator, int index)
+{
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length()-1;
 
+  for(int i=0; i<=maxIndex && found<=index; i++){
+    if(data.charAt(i)==separator || i==maxIndex){
+        found++;
+        strIndex[0] = strIndex[1]+1;
+        strIndex[1] = (i == maxIndex) ? i+1 : i;
+    }
+  }
 
+  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
 
-
+int stringToInt(String s)
+{
+    char arr[12];
+    s.toCharArray(arr, sizeof(arr));
+    return atoi(arr);
+}
